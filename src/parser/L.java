8be40b23dbdfb3ab2314118becf2bd08a574a454
Mekhrubon <mehrubon.97@mail.ru>
2016-12-28 01:14:29 +0300

    Самофокусировка добавлена. Исправлен *****код Вади и Сони.

--- src/parser/L.java
+++ src/parser/L.java
@@ -4,5 +4,13 @@ import model.*;
  * Created by vadim on 26.11.16.
  */
 public enum L {
-    val, var, plus, power, minus, mult, div, ln, open, close, end
+    val, var, plus('+'), power('^'), minus('-'), mult('*'), div('/'), ln, open('('), close(')'), end;
+
+    final char value;
+    L() {
+        value = 0;
+    }
+    L(char value) {
+        this.value = value;
+    }
 }
